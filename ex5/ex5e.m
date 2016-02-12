c4eb6d6765ec084e3aff2312ab19d6258f2dac0c
BourgondAries <macocio@gmail.com>
2016-02-13 00:14:08 +0100

    Add labels.

--- ex5/ex5e.m
+++ ex5/ex5e.m
@@ -1,17 +1,19 @@
 clc; clear all; hold on;
-dx = 1/2; a = 1; tstop = 10;
+dx = 1/2; a = 1; tstop = 1;
 legend_data = {}; count = 1;
-for dt = [1/6 1/5 1/4]
+for dt = [1/9 1/8 1/7]
 	u = [];
-	u(1) = 100;
-	r = a*dt/dx^2;
+	u(1) = 0;
+	r = a*dt/(dx^2);
 	steps = ceil(tstop / dt);
 	dt = tstop / steps;
-	legend_data{count} = num2str(r);
+	legend_data{count} = ['r=' num2str(r)];
 	count = count + 1;
 	for i = 1:steps
-		u(i+1) = r*(0-2*u(i)+0) + u(i);
+		u(i+1) = r*(50-2*u(i)+50) + u(i);
 	end
 	plot(0:dt:tstop, u);
 end
 legend(legend_data);
+ylabel('$K$', 'Interpreter', 'latex');
+xlabel('$t$', 'Interpreter', 'latex');
